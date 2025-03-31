5e5229f3f6a7c45ce16c8e460203219d81d92980
RossyTech <omadachinonso45@gmail.com>
2025-03-31 06:29:15 -0700

    added text-white to the mobilemenu

--- src/Mobilemenu.jsx
+++ src/Mobilemenu.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Mobilemenu({menuOpen, setMenuopen}) {
   return (
-  <div className={`fixed top-0 left-0 w-full bg-black-300 z-40 flex flex-col items-center justify-center
+  <div className={`fixed top-0 left-0 w-full text-white bg-black-300 z-40 flex flex-col items-center justify-center
   transition-all duration-300 ease-in-out
  ${
     menuOpen
